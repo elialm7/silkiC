4e319e9038a2caa054a90f7614419eed2e9065bc
elikawa7 <metall65eli@gmail.com>
2023-10-20 01:13:34 -0300

    added header for utilities

---
+++ src/headers/common.h
@@ -0,0 +1,7 @@
+#ifndef silk_common_h
+#define silk_common_h
+#include <stdbool.h> //contain macros for bool types 
+#include <stddef.h>// contains definitions for types, etc. 
+#include<stdint.h> //types for integers.
+
+#endif // !silk_common_h
