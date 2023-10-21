95a7c849f52d118fd38af027c0bb6affff17927e
elikawa7 <metall65eli@gmail.com>
2023-10-21 00:10:54 -0300

    testing the code.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,9 @@
 #include "chunk.h"
 
 int main(int argc, char const *argv[]){
-
-    printf("hello world \n");
-
-    /* code */
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
     return 0;
 }
