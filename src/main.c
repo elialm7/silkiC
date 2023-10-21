f52e4bd1ac08534249c8ceeaff52830bffc62f13
elikawa7 <metall65eli@gmail.com>
2023-10-21 01:05:05 -0300

    fixed the header and added the testing line for the disassambler

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
-#include "common.h"
 #include <stdio.h>
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, char const *argv[]){
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+    disassambleChunk(&chunk,"testing");
     freeChunk(&chunk);
     return 0;
 }
